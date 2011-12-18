af331f38df08e9c7fca905f4a51c6131107f8881
AJ <allysonjulian@gmail.com>
2011-12-17 18:26:16 -0600

    changed splash page to contain recent posts

--- _plugins/postmore.rb
+++ _plugins/postmore.rb
@@ -1,11 +1,11 @@
 module PostMore
   def postmorefilter(input, url, text)
       if input.include? "<!--more-->"
-            input.split("<!--more-->").first + "<p class='more'><a class='more' href='#{url}'>#{text}</a></p>"
-	        else
-		      input
-		          end
-			    end
-			    end
+         input.split("<!--more-->").first + "<p class='more'><a class='more' href='#{url}'>#{text}</a></p>"
+      else
+	 input
+      end
+	end
+	end
 
-			    Liquid::Template.register_filter(PostMore)
+Liquid::Template.register_filter(PostMore)
